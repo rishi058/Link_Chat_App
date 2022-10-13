5082f14e3699b402076052d3fdf293983992af4d
Rishi Raj <97884033+rishi058@users.noreply.github.com>
2022-10-13 17:04:21 +0530

    Add files via upload

---
+++ Constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:279016303622:web:d3695b2cd57510ac4a2787";
+  static String apiKey = "AIzaSyAfXwPspZcrRztQu20BicqKcu_5ex3Qx6g";
+  static String messagingSenderId = "279016303622";
+  static String projectId = "link-e5390";
+  final primaryColor = const Color(0xFFee7b64);
+}
